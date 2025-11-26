df32f3c5ccd38de0ea34f37cccfd416ec39e383d
ninon28 <ninon.gossiaux@gmail.com>
2025-11-26 10:06:05 +0100

    [tab] add animation

--- tabs/assets/app.js
+++ tabs/assets/app.js
@@ -26,4 +26,26 @@ function tabTrigger(event){
     const tabSelectID = event.currentTarget.getAttribute("href");
     const tabSelect = parentTab.querySelector(tabSelectID);
     tabSelect.classList.add("tab__content--active");
+
+    if(parentTab.classList.contains("tab--a")){
+        const tabContainerA = document.querySelector(".tab--a .tab__container");
+        tabContainerA.style.height = tabSelect.offsetHeight + "px"; //offset inclu la taille avec les paddings
+    }
+
+    if(parentTab.classList.contains("tab--b")){
+        const tabContainerB = document.querySelector(".tab--b .tab__container");
+        tabContainerB.style.height = tabSelect.offsetHeight + "px"; //offset inclu la taille avec les paddings
+    }
 }
+
+// définir la taille du container par rapport à la tab active
+const activeTabA = document.querySelector(".tab--a .tab__content--active");
+const tabContainerA = document.querySelector(".tab--a .tab__container");
+
+tabContainerA.style.height = activeTabA.offsetHeight + "px"; //offset inclu la taille avec les paddings
+
+// tabs B
+const activeTabB = document.querySelector(".tab--b .tab__content--active");
+const tabContainerB = document.querySelector(".tab--b .tab__container");
+
+tabContainerB.style.height = activeTabB.offsetHeight + "px"; //offset inclu la taille avec les paddings
